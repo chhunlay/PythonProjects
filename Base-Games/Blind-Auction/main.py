65f6e0c1c9f667b6daa76d8c67313ec0cd613e6d
chhunlay <chhunlayking@gmail.com>
2024-11-03 00:11:29 +0700

    chore: add logo in script

--- Base-Games/Blind-Auction/main.py
+++ Base-Games/Blind-Auction/main.py
@@ -14,7 +14,7 @@ def find_highest_bidder(bidding_record):
     bid_amount = bidding_record[bidder]
     if bid_amount > highest_bid: 
       highest_bid = bid_amount
-      winner = bidder
+      winner = bidder 
   print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 while not bidding_finished:
